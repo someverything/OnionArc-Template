40ec343ca985726ab8637c3a96a39856e680400b
someverything <rustameyvaz@gmail.com>
2024-08-30 23:45:49 +0400

    Add project files.

---
+++ Core/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Core/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Descriptiopn { get; set; }
+        public decimal Price { get; set; }
+        public Guid categoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
